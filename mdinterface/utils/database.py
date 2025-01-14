e12f2a574054a4663bef72bfd2bf578718a77a72
roncofaber <fabrice.roncoroni@gmail.com>
2025-01-13 18:13:35 -0800

    added metal111

--- mdinterface/utils/database.py
+++ mdinterface/utils/database.py
@@ -13,7 +13,7 @@ from mdinterface.core.specie import Specie
 from mdinterface.core.topology import Bond, Angle, Atom, Dihedral, Improper
 
 import ase.build
-
+from ase.build import fcc111
 #%%
 
 # graphene https://onlinelibrary.wiley.com/doi/10.1002/adma.201705791
@@ -108,4 +108,40 @@ class Perchlorate(Specie):
             }
 
         super().__init__(atoms=pclo, lj=lj, bonds=[b1, b2], angles=a1, cutoff=1.5, **kwargs)
+        return
+
+#metal parameters from https://pubs.acs.org/doi/full/10.1021/jp801931d
+class Metal111(Specie):
+    
+    def __init__(self, metal, **kwargs):
+        
+        slab = fcc111(metal, size=(1,2,3), orthogonal=True, periodic=True)
+        
+        # those are in kcal/mol and r0, so it should be divided by 2**1/6
+        metal_params = {
+            "Ag" : [4.56, 2.955],
+            "Al" : [4.02, 2.925],
+            "Au" : [5.29, 2.951],
+            "Cu" : [4.72, 2.616],
+            "Ni" : [5.65, 2.552],
+            "Pb" : [2.93, 3.565],
+            "Pd" : [6.15, 2.819],
+            "Pt" : [7.80, 2.845],
+            "Ac" : [6.51, 3.843],
+            "Ca" : [3.36, 4.025],
+            "Ce" : [6.38, 3.734],
+            "Es" : [2.88, 4.133],
+            "Ir" : [9.20, 2.785],
+            "Fe" : [6.00, 2.590],
+            "Rh" : [7.84, 2.757],
+            "Sr" : [3.40, 4.379],
+            "Th" : [8.47, 3.683],
+            "Yb" : [2.71, 3.942]
+        }
+        
+        lj = metal_params[metal]
+        lj_copy = [lj[0], round(lj[1] / np.power(2, 1/6), 5)]
+        
+        super().__init__(atoms=slab, lj=lj_copy, **kwargs)
+        
         return
