003386797beecbbc2163a88d6e5e651de2ae532e
roncofaber <fabrice.roncoroni@gmail.com>
2025-01-14 15:29:55 -0800

    bumped to ver 1.1.0

--- mdinterface/__init__.py
+++ mdinterface/__init__.py
@@ -1,9 +1,14 @@
 """
-mdinterface is a package to setup molecular dynamics systems.
+mdinterface: Build Interface Systems for Molecular Dynamics Simulations
+
+`mdinterface` is a Python package designed to build systems for Molecular Dynamics (MD) simulations.
+Initially developed to construct electrolyte/electrode interfaces, it is also well-suited for generating MD boxes of liquids,
+electrolyte systems, and polymer networks.
+
 """
 
-__version__ = '1.0.0'
-__date__ = '16 Dec. 2024'
+__version__ = '1.1.0'
+__date__ = '14 Jan. 2025'
 __author__ = 'Fabrice Roncoroni'
 __all__ = ['SimulationBox']
 
