d202738a75e2f5e3ee0ff7947aa352ad59e5e7b8
roncofaber <fabrice.roncoroni@gmail.com>
2026-03-09 18:53:06 -0700

    Bump version to 1.5.0

--- mdinterface/__init__.py
+++ mdinterface/__init__.py
@@ -6,8 +6,8 @@ Initially developed to construct electrolyte/electrode interfaces, it is also we
 
 """
 
-__version__ = '1.4.0'
-__date__ = '14 Oct. 2025'
+__version__ = '1.5.0'
+__date__ = '09 Mar. 2026'
 __author__ = 'Fabrice Roncoroni'
 __all__ = ['SimulationBox', 'BoxBuilder', "Specie", "Polymer"]
 
